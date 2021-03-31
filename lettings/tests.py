d90958d80bf10437c1441ec57af9ab4d43fa64f7
Florent <florentpeyre@hotmail.fr>
2021-03-31 19:33:48 +0200

    test ok

--- lettings/tests.py
+++ lettings/tests.py
@@ -4,7 +4,7 @@ from django.db.models.query import QuerySet
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
-from lettings.models import Letting
+from lettings.models import Letting, Address
 
 
 @pytest.mark.django_db
@@ -21,12 +21,15 @@ def test_index_view(client):
 
 @pytest.mark.django_db
 def test_letting_view(client):
-    letting = Letting.objects.get(pk=1)
-    assert letting.pk == 1
-    # letting_id = 1
-    # url = reverse('letting', args=(letting_id,))
-    # response = client.get(url)
-    # assert response.status_code == 200
-# assert type(response.context['title']), QuerySet
-# assert type(response.context['address']), QuerySet
-# assertTemplateUsed(response, 'lettings/letting.html')
+    adresse = Address.objects.create(number=7217, street='Bedford Street', city='Brunswick',
+                           state='GA', zip_code=31525, country_iso_code='USA')
+    letting = Letting.objects.create(title='Joshua Tree Green Haus /w Hot Tub', address_id=adresse.id)
+    url = reverse('letting', args=(letting.id,))
+    response = client.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    title = soup.find('title')
+    assert title.text == 'Joshua Tree Green Haus /w Hot Tub'
+    assert response.status_code == 200
+    assert type(response.context['title']), QuerySet
+    assert type(response.context['address']), QuerySet
+    assertTemplateUsed(response, 'lettings/letting.html')
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
@@ -13,7 +14,7 @@ def test_index_view(client):
     url = reverse('profiles_index')
     response = client.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    title =soup.find('title')
+    title = soup.find('title')
     assert title.text == 'Profiles'
     assert response.status_code == 200
     assert type(response.context['profiles_list']), QuerySet
@@ -22,8 +23,14 @@ def test_index_view(client):
 
 @pytest.mark.django_db
 def test_profile_view(client):
-    try:
-        profile = Profile.objects.get(pk=1)
-    except Profile.DoesNotExist:
-        profile = None
-    assert profile.pk == 1
+    user = User.objects.create(username='4meRomance', first_name='John', last_name="Rodriguez",
+                               email='coemperor@famemma.net')
+    Profile.objects.create(favorite_city='Buenos Aires', user_id=user.id)
+    url = reverse('profile', args=(user.username, ))
+    response = client.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    title = soup.find('title')
+    assert title.text == '4meRomance'
+    assert response.status_code == 200
+    assert type(response.context['profile']), QuerySet
+    assertTemplateUsed(response, 'profiles/profile.html')
